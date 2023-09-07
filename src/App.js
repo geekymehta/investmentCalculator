922cd0fc1b2aa955a268247ab92f4b38485a43d1
Sumit <136010191+sumitmehta021@users.noreply.github.com>
2023-09-07 21:24:59 +0000

    Update App.js and ResultsTable.js

--- src/App.js
+++ src/App.js
@@ -4,28 +4,32 @@ import UserInput from "./components/UserInput/UserInput";
 import ResultsTable from "./components/ResultsTable/ResultsTable";
 
 function App() {
-  const [results, setResults] = usestate(null); 
-  const calculateHandler = (userInput) => {
-    const yearlyData = [];
-    if(userInput){
-      let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-      const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
-      const expectedReturn = +userInput["expected-return"] / 100;
-      const duration = +userInput["duration"];
+ // const [results, setResults] = useState(null); 
+  //const [yearlyData, setYearlyData] = useState(); 
+  const [userInput, setUserInput] = useState(null)
 
-      for (let i = 0; i < duration; i++) {
-        const yearlyInterest = currentSavings * expectedReturn;
-        currentSavings += yearlyInterest + yearlyContribution;
-        yearlyData.push({
-          year: i + 1,
-          yearlyInterest: yearlyInterest,
-          savingsEndOfYear: currentSavings,
-          yearlyContribution: yearlyContribution
-        });
-      }
-    }
-    setResults(yearlyData);
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
   };
+
+  const yearlyData = [];
+  if(userInput){
+    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution
+      });
+    }
+  }
 
   return (
     <div>
@@ -34,7 +38,8 @@ function App() {
 
       <UserInput onCalculate={calculateHandler} />
 
-      <ResultsTable data = {yearlyData}/>
+      {!userInput && <p>No Input Yet.</p>}
+      {userInput && <ResultsTable data = {yearlyData} initialInvestment = {userInput["current-savings"]}/>}
 
     </div>
   );
